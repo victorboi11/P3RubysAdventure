f0c1f7b479a6b1c028960cff0cd26f6b2daeb979
victorboi11 <725304@student.dusd.net>
2023-11-17 11:42:29 -0800

    i added more code to rubys controller
    controller

--- RubysAdventureEstradaP3/Assets/Scripts/RubyController.cs
+++ RubysAdventureEstradaP3/Assets/Scripts/RubyController.cs
@@ -5,6 +5,11 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -12,6 +17,8 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -24,12 +31,17 @@ public class RubyController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
         transform.position = position;
 
         rigidbody2d.MovePosition(position);
     }
 
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
    
 }
